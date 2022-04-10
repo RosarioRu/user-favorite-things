408ddad32624b795dadab901c0a5ccc8acdd0ffd
RosarioRu <mariarruval@gmail.com>
2022-04-10 12:10:12 -0700

    Linked styles.css to html.
    Co-authored-by: Sun Roberts <suerobertstx@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,20 @@ $(document).ready(function() {
     const userFood=$("input#food").val();
     const userAnimal=$("input#animal").val();
 
-    let resultArray=[userName, userColor, userFood, userAnimal];
+    const resultArray=[userName, userColor, userFood, userAnimal];
+    const second=resultArray[1];
+    const first=resultArray[0];
+    const third=resultArray[2];
 
-    $("#insert").text(resultArray);
-    $(".show-results").show();
+    const newArray=[];
+    newArray.push(second, first, third);
+
+    $(".second").append(second);
+    $(".first").append(first);
+    $(".third").append(third);
+    $("#results-paragraph").show();
+    
+    
   });
 
 });
