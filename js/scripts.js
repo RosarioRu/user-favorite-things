94e0fab5ef44243660c6f1d2cd635d45abbd852e
RosarioRu <mariarruval@gmail.com>
2022-04-10 11:07:37 -0700

    Add functions to assign user inputs to corresponding variables.
    Co-authored-by: Sun Roberts <suerobertstx@yahoo.com>

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#favorite-things").submit(function(event) {
+    event.preventDefault();
+    const userName=$("input#name").val();
+    const userColor=$("input#color").val();
+    const userFood=$("input#food").val();
+    const userAnimal=$("input#animal").val();
+
+    
+    
+   
+
+    
+  });
+});
